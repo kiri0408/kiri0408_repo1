74686258e2286b86e73006610281d0c0209087c8
kiri0408 <98244264+kiri0408@users.noreply.github.com>
2022-04-13 22:27:34 +0900

    Add files via upload

--- excel.py
+++ excel.py
@@ -1,30 +1,63 @@
 from win32com.client import GetObject
 import pythoncom
+import pandas as pd
+from time import time
 
+#pywin32com
 #https://qiita.com/feo52/items/150745ae0cc17cb5c866
+
+
+#df のソート
+#https://note.nkmk.me/python-pandas-sort-values-sort-index/
+
+start1 = time()
 
 xl = GetObject(None, "Excel.Application")
 xl.Visible = True  # ビジブル設定
 xl.DisplayAlerts = False # 警告を非表示
 
+proc_time = time() - start1
+print("a:" , str(proc_time))
+
 wb = xl.Workbooks("test1.xlsx")
-ws = wb.Sheets("bbb")
-ws2 = wb.sheets("aaa")
+ws = wb.Sheets("ws")
+ws2 = wb.sheets("ws2")
 
-#セル値を2次元配列で取得：タプルで取得される
-ary1 = ws.Range(ws.cells(1,1),ws.cells(10000,100)).Value
-#print(f"{ary1=}")
+proc_time = time() - start1
+print("a:" , str(proc_time))
 
-ws2.Range(ws2.cells(1,1),ws2.cells(10000,100)).Value = ary1
+
+#Excel-tuple-df 取り込み  5列
+ary_col = ws.Range(ws.cells(1,5),ws.cells(1,9)).Value  #項目行をタプルに取得
+ary1 = ws.Range(ws.cells(5,5),ws.cells(8,9)).Value     #data行をタプルに取得
+df = pd.DataFrame(ary1, columns=[*ary_col[0]])         #tuple-list変換（項目行となるタプルの要素０を*で解答しリストに変換する）
+
+df = df[~df.duplicated(subset=['col1'])].reset_index(drop=True)   #dfの重複を除外
+print('-')
+print(df)
+
+#df-tuple-Excel出力 5列
+_tuple = [tuple(x) for x in df.values] #dfをタプルへ変換
+ws.Range(ws.cells(10,5),ws.cells(10+len(_tuple)-1,5+5-1)).Value = _tuple #tuple-Excel出力
+
+
+
+proc_time = time() - start1
+print("c:" , str(proc_time))
+
 
 ws.Range("A1:B4").Value = "値"
+
+
+xl.Visible = True  # ビジブル設定
+
 
 # A1からB列最終行まで一括して同じ値を入力
 #ws.Range(f"A1:B{endRow}").Value = "値"
 ws.Range(ws.Cells(1, 1), ws.Cells(30, 2)).Value = "値"
 
-xlUp = -4162   # VBAで"xlUp"を示す定数
-endRow = ws.Cells(ws.Rows.Count, 1).End(xlUp).Row
+#Excel最終行を取得
+endRow = ws.Cells(ws.Rows.Count, 1).End(-4162).Row #  xlUp = -4162   # VBAで"xlUp"を示す定数
 print(endRow)
 
 #セルにExcel関数を代入したい場合は.Formulaを使います
