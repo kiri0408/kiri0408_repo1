bd4e449786bece717c029a42117e2ab7a8f33436
kiri0408 <98244264+kiri0408@users.noreply.github.com>
2022-04-20 23:13:24 +0900

    Add files via upload

--- excel.py
+++ excel.py
@@ -1,4 +1,5 @@
-from win32com.client import GetObject
+#from win32com.client import GetObject
+import win32com.client
 import pythoncom
 import pandas as pd
 from time import time
@@ -6,51 +7,95 @@ from time import time
 #pywin32com
 #https://qiita.com/feo52/items/150745ae0cc17cb5c866
 
-
 #df のソート
 #https://note.nkmk.me/python-pandas-sort-values-sort-index/
 
 start1 = time()
 
-xl = GetObject(None, "Excel.Application")
-xl.Visible = True  # ビジブル設定
-xl.DisplayAlerts = False # 警告を非表示
-
 proc_time = time() - start1
 print("a:" , str(proc_time))
 
-wb = xl.Workbooks("test1.xlsx")
-ws = wb.Sheets("ws")
-ws2 = wb.sheets("ws2")
+#▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽
+#△△△△△△△△△△△△△△△△△△△△
 
-proc_time = time() - start1
-print("a:" , str(proc_time))
+try:
+    #EXCELオープン
+    #▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽
+    #xl = GetObject(None, "Excel.Application")
+    xl = win32com.client.Dispatch("Excel.Application")
+    from pathlib import Path
+    abspath = str(Path(r"C:\py\98_win32com\test33.xlsx").resolve())
+    wb = xl.Workbooks.Open(abspath, UpdateLinks=0, ReadOnly=False)  # 読み取り専用の場合は  ReadOnly=True
+    ws = wb.Sheets("ws")
+    ws2 = wb.sheets("ws2")
+    xl.DisplayAlerts = False # 警告を非表示
+    xl.Visible = False  #非表示
+    #△△△△△△△△△△△△△△△△△△△△△△△
+except:
+    print("エラー発生しました")
+    #EXCEL終了処理
+    #▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽
+    xl.DisplayAlerts = True # 警告を表示
+    xl.Visible = True       # 表示
+    #△△△△△△△△△△△△△△△△△△△△
 
+try:
+    #Excel-df 取り込み  
+    #▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽
+    startRow = 5
+    endRow = ws.Cells(ws.Rows.Count, 3).End(-4162).Row #  最終行取得  xlUp = -4162   # VBAで"xlUp"を示す定数
+    startCol = 3
+    endCol = 10
+    ary_col = ws.Range(ws.cells(startRow,startCol),ws.cells(startRow,endCol)).Value  #項目行をタプルに取得
+    ary1 = ws.Range(ws.cells(startRow+1,startCol),ws.cells(endRow,endCol)).Value     #data行をタプルに取得
+    df = pd.DataFrame(ary1, columns=[*ary_col[0]])         #tuple-list-df変換（項目行となるタプルの要素０を*で解凍しリストに変換する）
+    df = df.fillna('')           #NaNはエラーになることが多いのでfillnaしておく
+    #△△△△△△△△△△△△△△△△△△△△
+except:
+    print("エラー発生しました")
+    #EXCEL終了処理
+    #▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽
+    xl.DisplayAlerts = True # 警告を表示
+    xl.Visible = True       # 表示
+    #△△△△△△△△△△△△△△△△△△△△
 
-#Excel-tuple-df 取り込み  5列
-ary_col = ws.Range(ws.cells(1,5),ws.cells(1,9)).Value  #項目行をタプルに取得
-ary1 = ws.Range(ws.cells(5,5),ws.cells(8,9)).Value     #data行をタプルに取得
-df = pd.DataFrame(ary1, columns=[*ary_col[0]])         #tuple-list変換（項目行となるタプルの要素０を*で解答しリストに変換する）
-
-df = df[~df.duplicated(subset=['col1'])].reset_index(drop=True)   #dfの重複を除外
+print(df)
+#df.to_excel('df.xlsx')
+df = df[~df.duplicated(subset=['項目1'])].reset_index(drop=True)   #dfの重複を除外
 print('-')
 print(df)
 
-#df-tuple-Excel出力 5列
-_tuple = [tuple(x) for x in df.values] #dfをタプルへ変換
-ws.Range(ws.cells(10,5),ws.cells(10+len(_tuple)-1,5+5-1)).Value = _tuple #tuple-Excel出力
+try:
+    #df-Excel出力 
+    #▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽
+    startRow = 5
+    endRow = ws.Cells(ws.Rows.Count, 3).End(-4162).Row #  最終行取得  xlUp = -4162   # VBAで"xlUp"を示す定数
+    startCol = 3
+    endCol = 10
+    _tuple = [tuple(x) for x in df.values] #dfをタプルへ変換
+    ws2.Range(ws2.cells(startRow,startCol),ws2.cells(startRow+len(_tuple)-1,endCol-1)).Value = _tuple #tuple-Excel出力
+    #△△△△△△△△△△△△△△△△△△△△
 
+except:
+    print("エラー発生しました")
+    #EXCEL終了処理
+    #▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽
+    xl.DisplayAlerts = True # 警告を表示
+    xl.Visible = True       # 表示
+    #△△△△△△△△△△△△△△△△△△△△
 
+#EXCEL終了処理
+#▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽▽
+xl.DisplayAlerts = True # 警告を表示
+xl.Visible = True       # 表示
+#△△△△△△△△△△△△△△△△△△△△
 
 proc_time = time() - start1
 print("c:" , str(proc_time))
 
-
 ws.Range("A1:B4").Value = "値"
 
-
 xl.Visible = True  # ビジブル設定
-
 
 # A1からB列最終行まで一括して同じ値を入力
 #ws.Range(f"A1:B{endRow}").Value = "値"
