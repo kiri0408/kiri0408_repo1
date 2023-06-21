e11637669f10831eceddb83fc0cf11c6acc8167e
kiri0408 <98244264+kiri0408@users.noreply.github.com>
2023-06-21 21:54:20 +0900

    Update ファイル名のリスト化.py

--- ファイル名のリスト化.py
+++ ファイル名のリスト化.py
@@ -10,7 +10,7 @@ list2 = glob.glob(path)
 files=[]
 for file in list2:
     filename = Path(file).name  #ファイル名の取り出し
-    str1 = filename.split('_')  #ファイル名を分割
+    str1 = filename.split('_')  #ファイル名を分割  a
     files.append(str1)          #リスト化
 
 df = pd.DataFrame(files, columns=('a','b','c','d','e'))
