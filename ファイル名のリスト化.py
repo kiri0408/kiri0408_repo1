59c13a87b03be0016461e473da2408320eea3386
kiri0408 <98244264+kiri0408@users.noreply.github.com>
2023-06-21 21:52:47 +0900

    Add files via upload

--- ファイル名のリスト化.py
+++ ファイル名のリスト化.py
@@ -1,6 +1,7 @@
 import glob
 import pandas as pd
 from pathlib import Path
+import datetime
 
 #指定フォルダから ファイル名の一覧を取得 
 path = r"C:\py\56_ファイル名リスト化\fol\*.txt"
@@ -13,4 +14,17 @@ for file in list2:
     files.append(str1)          #リスト化
 
 df = pd.DataFrame(files, columns=('a','b','c','d','e'))
+df['count']=''
+
+for i in range(len(df)):
+    try:
+        df.loc[i,'date'] = datetime.datetime.strptime( df.loc[i,'b'] , '%Y-%m-%d')
+        df.loc[i,'month'] = df.loc[i,'date'].strftime('%Y%m')
+    except:
+        False
+
+#df2 = df[['a','month','count']].groupby(['a','month'], as_index=False).count()
+df2 = pd.pivot_table(df,index='a',columns='month',values='count',aggfunc='count')
+#pd.pivot_table(df, index=’日付’, columns=’商品名’,values=’販売数量’,aggfunc=’sum’)
 print(df)
+print(df2)
