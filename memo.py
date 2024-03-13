edfb6262efaa552c0de366e68d6c9d764d444050
kiri0408 <98244264+kiri0408@users.noreply.github.com>
2024-03-13 21:38:44 +0900

    Add files via upload

---
+++ memo.py
@@ -0,0 +1,8 @@
+# https://nets-tip.com/?p=1313
+#
+# Apache Tomcat の起動と停止
+# 起動：「C:{Tomcatインストールディレクトリ}\bin\startup.bat」をダブルクリック
+# 停止：「C:{Tomcatインストールディレクトリ}\bin\shutdown.bat」をダブルクリック
+#
+# Tomcatサービス登録
+# C:\tomcat\bin\service.bat install Tomcat9
