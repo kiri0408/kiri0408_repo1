6d07b1d82abaaed676d2b730810c10ab6cd11537
kiri0408 <98244264+kiri0408@users.noreply.github.com>
2024-03-14 20:38:13 +0900

    Add files via upload

--- filecopy.py
+++ filecopy.py
@@ -3,19 +3,24 @@ import glob
 import shutil
 import os
 
+
+print('処理開始')
 ori  =r'C:\py\53_ファイルコピー/b'
 dist =r'C:\py\53_ファイルコピー\dist'
 
 # 条件に合うファイルパス情報をすべて取得
 file_list = glob.glob(  ori + r"/**/*", recursive=True)
 
-
+i=0
+j=0
 for file in file_list:
+    i+=1
+    print(f'{i}/{len(file_list)} ： {file} ')
     if os.path.isfile(file):
-        print('コピーします：', file)
         shutil.copy2(file, dist)
+        j+=1
 
-print('処理終了')
+print(f'処理終了。  ファイル数は{j}個です。   ')
 
 
 
